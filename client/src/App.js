cbc0c5d48897b2f0350c94f0f6750ec7e1afac18
Richard Houck <rdhouck@gmail.com>
2020-02-20 15:21:25 -0800

    updated stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
-import Todo from './components/pages/Todo/Todo.js';
+// import Todo from './components/pages/Todo/Todo.js';
 import TodoList from './components/pages/TodoList/TodoList.js';
 import Blog from './components/pages/Blog/Blog.js';
 import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
@@ -16,7 +16,7 @@ class App extends Component {
         <nav className="App-navigation">
           <h1 className="App-title">MERN Starter</h1>
           <Link to="/">Welcome</Link>
-          <Link to="/todo/">Todo</Link>
+          {/* <Link to="/todo/">Todo</Link> */}
           <Link to="/todolist/">TodoList</Link>
           <Link to="/blog/">Blog</Link>
           <Link to="/write/">Write Article</Link>
@@ -25,7 +25,7 @@ class App extends Component {
         <div className="App-mainContent">
           <Switch>
             <Route exact path='/' component={LandingPage} />
-            <Route exact path='/todo/' component={Todo} />
+            {/* <Route exact path='/todo/' component={Todo} /> */}
             <Route exact path='/todolist/' component={TodoList} />
             <Route exact path='/blog/' component={Blog} />
             <Route exact path='/write/' component={WriteArticle} />
--- foo.sh
+++
@@ -1,5 +0,0 @@
-git remote add origin https://github.com/acanlord/react-todo.git
-git push -u origin masterecho "# rich-todo" >> README.md
-git add README.md
-git remote add origin https://github.com/acanlord/rich-todo.git
-git push -u origin master
