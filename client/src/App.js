75d108c66829a6cacb5cf14536a71aa10239297f
Richard Houck <rdhouck@gmail.com>
2020-02-25 20:49:31 -0800

    updated stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,7 @@ import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
-// import TodoList from './components/pages/TodoList/TodoList.js';
-// import Blog from './components/pages/Blog/Blog.js';
-// import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
 import Todo from './components/pages/TodoList/Todo.js';
-// import TodoList from './components/pages/TodoList/TodoList.js';
 
 class App extends Component {
   render() {
@@ -17,21 +13,13 @@ class App extends Component {
         <nav className="App-navigation">
           <h1 className="App-title">MERN Starter</h1>
           <Link to="/">Welcome</Link>
-          {/* <Link to="/todoList/">TodoList</Link> */}
           <Link to="/todo/">Todo</Link>
-          {/* <Link to="/todo/">Todo</Link> */}
-          {/* <Link to="/blog/">Blog</Link> */}
-          {/* <Link to="/write/">Write Article</Link> */}
         </nav>
 
         <div className="App-mainContent">
           <Switch>
             <Route exact path='/' component={LandingPage} />
-            {/* <Route exact path='/todoList/' component={TodoList} /> */}
-            {/* <Route exact path='/todo/' component={Todo} /> */}
             <Route exact path='/todo/' component={Todo} />
-            {/* <Route exact path='/blog/' component={Blog} /> */}
-            {/* <Route exact path='/write/' component={WriteArticle} /> */}
           </Switch>
         </div>
 
